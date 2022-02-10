262e2f15a264c1c39243c047785960ca6dab4061
Adam Henriksson <adam.ingemar.henriksson@gmail.com>
2022-02-10 22:31:46 +0100

    Initial commit

---
+++ index.ts
@@ -0,0 +1,49 @@
+// dependencies
+import path from "path";
+import express, { Request, Response } from "express";
+import { execSync } from "child_process";
+
+const app = express();
+
+app.get("build", (req: Request, res: Response) => {
+  // here you do all the continuous integration tasks
+  // for example
+  // 1st clone your repository
+
+  const user = "DD2480-Group-18";
+  const repo_name = "Continuous-Integration";
+  const branch = "master";
+
+  execSync(
+    `git clone git@github.com:${user}/${repo_name}.git --branch ${branch}`,
+    {
+      stdio: [0, 1, 2], // we need this so node will print the command output
+      cwd: path.resolve(__dirname, ""), // path to where you want to save the file
+    }
+  );
+
+  console.log(
+    `done: git clone git@github.com:${user}/${repo_name}.git --branch ${branch}`
+  );
+
+  execSync(`cd ${repo_name}`, {
+    stdio: [0, 1, 2], // we need this so node will print the command output
+    cwd: path.resolve(__dirname, ""), // path to where you want to save the file
+  });
+
+  console.log(`done: cd ${repo_name}`);
+
+  execSync("npx tsc", {
+    stdio: [0, 1, 2], // we need this so node will print the command output
+    cwd: path.resolve(__dirname, ""), // path to where you want to save the file
+  });
+
+  console.log("done: npx tsc");
+});
+
+// PORT
+var PORT = 80;
+app.listen(PORT, function () {
+  console.log(`Server is running on PORT: ${PORT}`);
+  return;
+});
