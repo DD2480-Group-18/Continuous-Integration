ae080c74f9069b1e16305d155470958bd8e16ea1
Adam Henriksson <adam.ingemar.henriksson@gmail.com>
2022-02-11 04:59:01 +0100

    spelling

--- index.ts
+++ index.ts
@@ -14,7 +14,7 @@ import axios from "axios";
 const app = express();
 app.use(bodyParser.json());
 
-// Configs
+// config
 const PORT = 80;
 const CMD_EXEC_OPTIONS = [0, 1, 2];
 const JOB_FILE_DIR = "ci-jobs";
