f84f63347a9cb24c8ea23b117bd1b0c4104873b3
Adam Henriksson <adam.ingemar.henriksson@gmail.com>
2022-02-11 04:28:27 +0100

    added API calls for check started and check completed

--- index.ts
+++ index.ts
@@ -1,63 +1,106 @@
 import express, { Request, Response } from "express";
-import bodyParser from "body-parser"
-import { CIConfig, WebhookBody } from "./types";
+import bodyParser from "body-parser";
+import { CheckRunBody, CIConfig, WebhookBody } from "./types";
 import { executeAndLogCommand } from "./tools";
-import { readFile, rmdir } from 'fs/promises';
-import path = require("path")
+import { readFile, rmdir } from "fs/promises";
+import path = require("path");
 
 const app = express();
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 const PORT = 80;
-const CMD_EXEC_OPTIONS = [0, 1, 2]
-const JOB_FILE_DIR = "ci-jobs"
-const CI_FILE_NAME = ".ci.json"
+const CMD_EXEC_OPTIONS = [0, 1, 2];
+const JOB_FILE_DIR = "ci-jobs";
+const CI_FILE_NAME = ".ci.json";
 
 app.post("/run", async (req: Request, res: Response) => {
   const {
     repository: {
       ssh_url,
       name: repositoryName,
-      owner: {
-        name: ownerName
-      }
+      owner: { name: ownerName },
     },
     after: commitHash,
-    ref: branchRef
-  }: WebhookBody = req.body
-  
-  // extract the clean branch name of the commit
-  const branch = branchRef.substring("refs/heads/".length)
+    ref: branchRef,
+  }: WebhookBody = req.body;
 
-  const jobDirectory = `${JOB_FILE_DIR}/${ownerName}-${repositoryName}-${commitHash}`
+  // Set as in-progress
+  const commit_check_url = `https://github.com/repos/${ownerName}/${repositoryName}/check-runs`;
+  const commit_check_name = "code-coverage";
+
+  const set_in_progress_body: CheckRunBody = {
+    name: commit_check_name,
+    head_sha: branchRef,
+  };
+  fetch(commit_check_url, {
+    method: "POST",
+    headers: {
+      "content-type": "application/vnd.github.v3+json",
+    },
+    body: JSON.stringify(set_in_progress_body),
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+
+  // extract the clean branch name of the commit
+  const branch = branchRef.substring("refs/heads/".length);
+
+  const jobDirectory = `${JOB_FILE_DIR}/${ownerName}-${repositoryName}-${commitHash}`;
 
   // create and enter parent and job directory (if not existing)
-  const createDirsCommand = `mkdir -p "${jobDirectory}"`
+  const createDirsCommand = `mkdir -p "${jobDirectory}"`;
   executeAndLogCommand(createDirsCommand, CMD_EXEC_OPTIONS);
 
   // clone repository into current directory
-  const cloneCommand = `git clone ${ssh_url} . --branch ${branch}`
+  const cloneCommand = `git clone ${ssh_url} . --branch ${branch}`;
   executeAndLogCommand(cloneCommand, CMD_EXEC_OPTIONS, jobDirectory);
-  
+
   // read .ci.json configuration file and run the user-defined steps
   const absoluteJobDirectory = path.resolve(__dirname, jobDirectory);
-  const ciConfigFileBuffer = await readFile(path.join(absoluteJobDirectory, CI_FILE_NAME));
+  const ciConfigFileBuffer = await readFile(
+    path.join(absoluteJobDirectory, CI_FILE_NAME)
+  );
   if (!ciConfigFileBuffer) {
     res.status(500);
     return;
   }
-  const { dependencies, compile, test }: CIConfig = JSON.parse(ciConfigFileBuffer.toString())
+  const { dependencies, compile, test }: CIConfig = JSON.parse(
+    ciConfigFileBuffer.toString()
+  );
 
   // run dependency installation steps
-  dependencies.forEach((cmd) => executeAndLogCommand(cmd, CMD_EXEC_OPTIONS, absoluteJobDirectory))
+  dependencies.forEach((cmd) =>
+    executeAndLogCommand(cmd, CMD_EXEC_OPTIONS, absoluteJobDirectory)
+  );
   // run compilation steps
-  compile.forEach((cmd) => executeAndLogCommand(cmd, CMD_EXEC_OPTIONS, absoluteJobDirectory))
+  compile.forEach((cmd) =>
+    executeAndLogCommand(cmd, CMD_EXEC_OPTIONS, absoluteJobDirectory)
+  );
   // run testing steps
-  test.forEach((cmd) => executeAndLogCommand(cmd, CMD_EXEC_OPTIONS, absoluteJobDirectory))
+  test.forEach((cmd) =>
+    executeAndLogCommand(cmd, CMD_EXEC_OPTIONS, absoluteJobDirectory)
+  );
 
   // cleanup build files
-  const rmCommand = `rm -rf "${absoluteJobDirectory}"`
+  const rmCommand = `rm -rf "${absoluteJobDirectory}"`;
   executeAndLogCommand(rmCommand, CMD_EXEC_OPTIONS);
+
+  // success
+  const set_success_body: CheckRunBody = {
+    name: commit_check_name,
+    head_sha: branchRef,
+    status: "completed",
+    conclusion: "success",
+  };
+  await fetch(commit_check_url, {
+    method: "POST",
+    headers: {
+      "content-type": "application/vnd.github.v3+json",
+    },
+    body: JSON.stringify(set_success_body),
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
 });
 
 app.listen(PORT, function () {
--- types.ts
+++ types.ts
@@ -1,17 +1,55 @@
 export type WebhookBody = {
   repository: {
-    ssh_url: string,
-    name: string,
+    ssh_url: string;
+    name: string;
     owner: {
-      name: string
-    },
-  },
-  after: string,
-  ref: string
-}
+      name: string;
+    };
+  };
+  after: string;
+  ref: string;
+};
 
 export type CIConfig = {
-  dependencies: string[],
-  compile: string[],
-  test: string[],
-}
+  dependencies: string[];
+  compile: string[];
+  test: string[];
+};
+
+export type CheckRunBody = {
+  name: string;
+  head_sha: string;
+  details_url?: string;
+  external_id?: string;
+  status?: "queued" | "in_progress" | "completed";
+  started_at?: string;
+  conclusion?: string;
+  completed_at?: string;
+  output?: Output;
+};
+
+type Output = {
+  title: string;
+  summary: string;
+  text?: string;
+  annotations?: Annotation[];
+  images?: Image[];
+};
+
+type Annotation = {
+  path: string;
+  start_line: number;
+  end_line: number;
+  start_column?: number;
+  end_column?: number;
+  annotation_level: "notice" | "warning" | "failure";
+  message: string;
+  title?: string;
+  raw_details: string;
+};
+
+type Image = {
+  alt: string;
+  image_url: string;
+  caption?: string;
+};
