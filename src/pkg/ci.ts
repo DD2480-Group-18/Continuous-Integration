869877692a66d6b6e8c08dd3e55732efbac03670
Zino Kader <zinokad@gmail.com>
2022-02-15 04:46:33 -0600

    feat: create temporary CI files in top-level directory (#11)
    Closes #10
    
    Changes:
    
    * feat: run file commands relative to the project root directory
    
    * feat: add testing configurations for new top-level structure
    
    * fix: executeAndLogCommands appended root directory to already absolute path
    
    * fix: cwd used as empty if not provided caused errors with filepaths with whitespace

--- src/pkg/ci.ts
+++ src/pkg/ci.ts
@@ -6,47 +6,48 @@ import { executeAndLogCommand } from "./io";
 
 /**
  * Creates a parent- (if not existing) and job-directory
- * 
+ *
  * @param jobDirectory the relative path of the directory to create
  */
 export const createJobDirectory = (jobDirectory: string) => {
   const createDirsCommand = `mkdir -p "${jobDirectory}"`;
   executeAndLogCommand(createDirsCommand, CMD_EXEC_OPTIONS);
-}
+};
 
 /**
  * Clones repository into the specified directory
- * 
+ *
  * @param sshURL the SSH URL of the git repository to clone
  * @param branchRef the reference name of the branch to clone
  * @param intoDirectory the directory to clone the repository into
  */
-export const cloneRepository = (sshURL: string, branchRef: string, intoDirectory: string) => {
+export const cloneRepository = (
+  sshURL: string,
+  branchRef: string,
+  intoDirectory: string
+) => {
   const branch = branchRef.substring("refs/heads/".length);
   const cloneCommand = `git clone ${sshURL} . --branch ${branch}`;
   executeAndLogCommand(cloneCommand, CMD_EXEC_OPTIONS, intoDirectory);
-}
+};
 
 /**
  * Reads the CI config file and returns its contents
- * 
+ *
  * @param jobDirectory the path to the directory of the CI job
  * @returns the CI config file, or null if it did not exist
  */
 export const getRepositoryConfig = async (jobDirectory: string) => {
-  const absoluteJobDirectory = path.resolve(__dirname, jobDirectory);
-  const configFilePath = path.join(absoluteJobDirectory, CI_FILE_NAME);
+  const configFilePath = path.join(jobDirectory, CI_FILE_NAME);
   const ciConfigFileBuffer = await readFile(configFilePath);
   if (!ciConfigFileBuffer) return null;
 
-  const config: CIConfig = JSON.parse(
-    ciConfigFileBuffer.toString()
-  );
-  return config
-}
+  const config: CIConfig = JSON.parse(ciConfigFileBuffer.toString());
+  return config;
+};
 
 /**
- * 
+ *
  */
 export const runCISteps = (steps: string[], workingDirectory: string) => {
   let succeeded = true;
@@ -54,4 +55,4 @@ export const runCISteps = (steps: string[], workingDirectory: string) => {
     executeAndLogCommand(step, CMD_EXEC_OPTIONS, workingDirectory);
   }
   return succeeded;
-}
+};
