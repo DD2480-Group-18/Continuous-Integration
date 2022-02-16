9e234a3ed12f628e14ef5d23bf5ad67b5625f4d7
Adam Henriksson <adam.ingemar.henriksson@gmail.com>
2022-02-16 14:14:31 +0100

    Test repo setup

--- src/constants/constants.ts
+++
@@ -1,8 +0,0 @@
-export const PORT = 3000;
-
-export const JOB_FILE_DIR = "ci-jobs";
-export const RESULTS_FILE_DIR = "ci-results";
-
-export const CI_FILE_NAME = ".ci.json";
-export const METADATA_FILE_NAME = "meta.json";
-export const OUTPUT_LOG_FILE_NAME = "out.log";
--- src/types/node.d.ts
+++
@@ -1,5 +0,0 @@
-declare namespace NodeJS {
-  export interface ProcessEnv {
-    GITHUB_CI_ACCESS_TOKEN: string;
-  }
-}
